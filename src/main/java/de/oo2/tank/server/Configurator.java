2949e2dbe972babbda9729e52253b1a27a52296d
Peter <p.boehringer@gmail.com>
2016-08-09 08:49:16 +0200

    additional properties for the configurator

--- src/main/java/de/oo2/tank/server/Configurator.java
+++ src/main/java/de/oo2/tank/server/Configurator.java
@@ -8,16 +8,24 @@ public final class Configurator {
     // keys for the environment variables
     public static final String KEY_CLIENT_RESSOURCE_BASE = "TANK_CLIENT_RESSOURCE_BASE";
     public static final String KEY_SERVER_PORT = "TANK_SERVER_PORT";
-
     public static final String KEY_DATABASE_NAME = "TANK_DATABASE_NAME";
+
+    // DB Host
+    // DB Port
+    // DB Name "test, development, ..."
     public static final String KEY_DATABASE_USER = "TANK_DATABASE_USER";
     public static final String KEY_DATABASE_PASSWORD = "TANK_DATABASE_PASSWORD";
-
     // default values of the environment variables
     public static final String DEFAULT_CLIENT_RESSOURCE_BASE = "WebClient/index.html";
     public static final String DEFAULT_SERVER_PORT = "8080";
     public static final String DEFAULT_DATABASE_NAME = "development";
     public static final String DEFAULT_DATABASE_USER = "dev";
     public static final String DEFAULT_DATABASE_PASSWORD = "dev";
+    private String serverPort;
+
+    // Google Analytics
+    // Loggly
+    // API Key
+
 
 }
