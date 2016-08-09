1585532d76dca0a736cfb53514eceaa2e0e259da
Peter <p.boehringer@gmail.com>
2016-08-09 10:24:28 +0200

    improved documentations and comments

--- src/main/java/de/oo2/tank/server/Server.java
+++ src/main/java/de/oo2/tank/server/Server.java
@@ -1,6 +1,5 @@
 package de.oo2.tank.server;
 
-import de.oo2.tank.server.dao.MeasurementDao;
 import io.swagger.annotations.Contact;
 import io.swagger.annotations.Info;
 import io.swagger.annotations.SwaggerDefinition;
@@ -8,9 +7,6 @@ import io.swagger.annotations.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-
-import static de.oo2.tank.server.Configurator.*;
 import static de.oo2.tank.server.JsonUtil.toJson;
 import static spark.Spark.*;
 
@@ -30,29 +26,20 @@ public class Server {
     private static final Logger logger = LoggerFactory.getLogger(Server.class.getName());
 
     /**
-     * main() Method as the application entry point
+     * Main method as the application entry point.
      *
      * @param args command line arguments
      */
     public static void main(String[] args) {
+        Configurator config = new Configurator();
+
         logger.info("Starting the server.");
 
-        Map<String, String> env = System.getenv();
-        env.getOrDefault(KEY_CLIENT_RESSOURCE_BASE, DEFAULT_CLIENT_RESSOURCE_BASE);
-        env.getOrDefault(KEY_SERVER_PORT, DEFAULT_SERVER_PORT);
-        env.getOrDefault(KEY_DATABASE_NAME, DEFAULT_DATABASE_NAME);
-        env.getOrDefault(KEY_DATABASE_USER, DEFAULT_DATABASE_USER);
-        env.getOrDefault(KEY_DATABASE_PASSWORD, DEFAULT_DATABASE_PASSWORD);
-
-        port(8080);
+        port(config.getServerPort());
 
         staticFiles.location("/public");
 
-        // String dbNamne = (String) app.getProperties().getOrDefault(KEY_DATABASE_NAME, "test");
-        MeasurementDao dao = new MeasurementDao("test", "docker.local", 27017);
-        // MeasurementDao dao = new MeasurementDao("test", "localhost", 27017);
-
-        new TemperatureRoutes(new TemperatureService(dao));
+        new TemperatureRoutes(new TemperatureService(config));
 
 
         try {
@@ -63,17 +50,18 @@ public class Server {
             });
 
         } catch (Exception e) {
-            System.err.println(e);
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
 
         // after each route
         after((req, res) -> {
             res.type("application/json");
 
-            // https://github.com/brsanthu/google-analytics-java
-            // GoogleAnalytics ga = new GoogleAnalytics("UA-12345678-1");
-            // ga.postAsync(new PageViewHit("https://www.xxx.com", "api"));
+            if (config.getGoogleAnalyticsKey() != null) {
+                // https://github.com/brsanthu/google-analytics-java
+                // GoogleAnalytics ga = new GoogleAnalytics("UA-12345678-1");
+                // ga.postAsync(new PageViewHit("https://www.xxx.com", "api"));
+            }
 
         });
 
---
+++ src/test/java/de/oo2/tank/server/dao/ConfiguratorTest.java
@@ -0,0 +1,59 @@
+package de.oo2.tank.server.dao;
+
+import de.oo2.tank.server.Configurator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class for the configurator.
+ */
+public class ConfiguratorTest {
+    private Configurator config;
+
+    @Before
+    public void setUp() {
+        config = new Configurator();
+    }
+
+    @After
+    public void tearDown() {
+        config = null;
+    }
+
+    @Test
+    public void testServerPort() {
+        Assert.assertEquals(new Integer(8080), config.getServerPort());
+    }
+
+    @Test
+    public void testDbHost() {
+        Assert.assertEquals("localhost", config.getDbHost());
+    }
+
+    @Test
+    public void testDbPort() {
+        Assert.assertEquals(new Integer(27017), config.getDbPort());
+    }
+
+    @Test
+    public void testDbName() {
+        Assert.assertEquals("test", config.getDbName());
+    }
+
+    @Test
+    public void testTankApiKey() {
+        Assert.assertEquals("ABC123", config.getTankApiKey());
+    }
+
+    @Test
+    public void testGoogleAnalyticsKey() {
+        Assert.assertNull(config.getGoogleAnalyticsKey());
+    }
+
+    @Test
+    public void testLogglyKey() {
+        Assert.assertNull(config.getLogglyKey());
+    }
+}
