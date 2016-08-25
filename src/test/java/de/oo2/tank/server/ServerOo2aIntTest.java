1027a9bce09c400bda879096e8d8989fa94dbdd7
Peter <p.boehringer@gmail.com>
2016-08-25 19:01:10 +0200

    inplemented test

--- src/test/java/de/oo2/tank/server/ServerOo2aIntTest.java
+++ src/test/java/de/oo2/tank/server/ServerOo2aIntTest.java
@@ -1,10 +1,12 @@
 package de.oo2.tank.server;
 
 import com.google.gson.Gson;
+import de.oo2.tank.server.model.ResponseError;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.ContentResponseHandler;
 import org.apache.http.client.fluent.Request;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ServerOo2aIntTest {
@@ -12,16 +14,18 @@ public class ServerOo2aIntTest {
 
     @Test
     public void testGet() throws Exception {
-        HttpResponse httpResponse = Request.Get("https://google.de")
+        HttpResponse httpResponse = Request.Get("https://www.oo2a.de/api/tank/temperatures/54651022bffebc03098b4567")
                 .execute()
                 .returnResponse();
 
         int code = httpResponse.getStatusLine().getStatusCode();
 
-        Content content = new ContentResponseHandler().handleEntity(httpResponse.getEntity());
-        // ResponseError errorMessage = gson.fromJson(content.toString(), ResponseError.class);
+        Assert.assertEquals(400, code);
 
-        System.out.println(content.toString());
+        Content content = new ContentResponseHandler().handleEntity(httpResponse.getEntity());
+        ResponseError errorMessage = gson.fromJson(content.toString(), ResponseError.class);
+
+        Assert.assertEquals("No temperature measurement with id '54651022bffebc03098b4567' found!", errorMessage.getMessage());
 
     }
 
