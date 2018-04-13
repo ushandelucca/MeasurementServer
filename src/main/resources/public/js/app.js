a9d3da62ff3d5f603f78b4ad533453edcda8766d
Peter Boehringer <p.boehringer@gmail.com>
2018-04-13 08:13:50 +0200

    Select time series for the measurements

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -35,14 +35,25 @@ var chartData = {
     }]
 };
 
-// Register event handlers
-$('#action-1-week').click(function (e) {
-  e.preventDefault();
+// Update the chart with the data from begin to the end.
+function updateChart(begin, end) {
 
-  var begin = moment().subtract(3, 'days').format("YYYY-MM-DD");
-  var end = moment().format("YYYY-MM-DD");
+  $.getJSON( "/api/tank/measurements?query=return&sensor=TankLevel&begin=" + begin + "&end=" + end + "&sort=-date", function( data ) {
+
+    levelSeries = [];
+
+    $.each( data, function( key, val ) {
+      var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A").format("YYYY-MM-DD HH:mm:ss");
+      levelSeries.push({x:ts, y:val.value});
+    });
+
+    chartData.datasets[0].data = levelSeries;
+    chart.update();
+  });
 
   $.getJSON( "/api/tank/measurements?query=return&sensor=Precipitation&begin=" + begin + "&end=" + end + "&sort=-date", function( data ) {
+
+    rainSeries = [];
 
     $.each( data, function( key, val ) {
       var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A").format("YYYY-MM-DD HH:mm:ss");
@@ -52,7 +63,41 @@ $('#action-1-week').click(function (e) {
     chartData.datasets[1].data = rainSeries;
     chart.update();
   });
+}
 
-  console.log("Event: " + e.value);
+// Register event handlers for the chart update.
+$('#action-3-days').click(function (e) {
+  e.preventDefault();
 
+  var begin = moment().subtract(3, 'days').format("YYYY-MM-DD");
+  var end = moment().format("YYYY-MM-DD");
+
+  updateChart(begin, end);
+})
+
+$('#action-1-week').click(function (e) {
+  e.preventDefault();
+
+  var begin = moment().subtract(7, 'days').format("YYYY-MM-DD");
+  var end = moment().format("YYYY-MM-DD");
+
+  updateChart(begin, end);
+})
+
+$('#action-1-month').click(function (e) {
+  e.preventDefault();
+
+  var begin = moment().subtract(1, 'months').format("YYYY-MM-DD");
+  var end = moment().format("YYYY-MM-DD");
+
+  updateChart(begin, end);
+})
+
+$('#action-6-months').click(function (e) {
+  e.preventDefault();
+
+  var begin = moment().subtract(6, 'months').format("YYYY-MM-DD");
+  var end = moment().format("YYYY-MM-DD");
+
+  updateChart(begin, end);
 })
