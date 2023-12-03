074e731c3d93e486ae2e15c43eed4d06cb3a3123
Peter <p.boehringer@gmail.com>
2023-12-03 19:27:23 +0100

    format

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -7,57 +7,59 @@ var chart;
 var rainSeries = [];
 var levelSeries = [];
 
-    window.chartColors = {
-        red: 'rgb(255, 99, 132)',
-        orange: 'rgb(255, 159, 64)',
-        yellow: 'rgb(255, 205, 86)',
-        green: 'rgb(92, 184, 92)',
-        blue: 'rgb(51, 122, 183)',
-        purple: 'rgb(153, 102, 255)',
-        grey: 'rgb(231,233,237)'
-    };
+window.chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(92, 184, 92)',
+  blue: 'rgb(51, 122, 183)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(231,233,237)'
+};
 
 var color = Chart.helpers.color;
 var chartData = {
-    datasets: [{
-        label: "Wasserstand",
-        borderColor: window.chartColors.green,
-        backgroundColor: color(window.chartColors.green).alpha(0.2).rgbString(),
-        yAxisID: "y-axis-1",
-        data: []
-        },
-        {
-        label: "Regen",
-        borderColor: window.chartColors.blue,
-        backgroundColor: color(window.chartColors.blue).alpha(0.2).rgbString(),
-        yAxisID: "y-axis-2",
-        data: []
-    }]
+  datasets: [{
+    label: "Wasserstand",
+    borderColor: window.chartColors.green,
+    backgroundColor: color(window.chartColors.green).alpha(0.2).rgbString(),
+    yAxisID: "y-axis-1",
+    data: []
+  },
+  {
+    label: "Regen",
+    borderColor: window.chartColors.blue,
+    backgroundColor: color(window.chartColors.blue).alpha(0.2).rgbString(),
+    yAxisID: "y-axis-2",
+    data: []
+  }]
 };
 
 // Update the chart with the data from begin to the end.
 function updateChart(begin, end) {
 
-  $.getJSON( "/api/tank/measurements?query=return&sensor=TankLevel&begin=" + begin + "&end=" + end + "&sort=-date", function( data ) {
+  $.getJSON("/api/tank/measurements?query=return&sensor=TankLevel&begin=" + begin + "&end=" + end + "&sort=-date", function (data) {
 
     levelSeries = [];
 
-    $.each( data, function( key, val ) {
-      var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A").format("YYYY-MM-DD HH:mm:ss");
-      levelSeries.push({x:ts, y:val.value});
+    $.each(data, function (key, val) {
+      //      var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A").format("YYYY-MM-DD HH:mm:ss");
+      var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A");
+      levelSeries.push({ x: ts, y: val.value });
     });
 
     chartData.datasets[0].data = levelSeries;
     chart.update();
   });
 
-  $.getJSON( "/api/tank/measurements?query=return&sensor=Precipitation&begin=" + begin + "&end=" + end + "&sort=-date", function( data ) {
+  $.getJSON("/api/tank/measurements?query=return&sensor=Precipitation&begin=" + begin + "&end=" + end + "&sort=-date", function (data) {
 
     rainSeries = [];
 
-    $.each( data, function( key, val ) {
-      var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A").format("YYYY-MM-DD HH:mm:ss");
-      rainSeries.push({x:ts, y:val.value});
+    $.each(data, function (key, val) {
+      //      var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A").format("YYYY-MM-DD HH:mm:ss");
+      var ts = moment(val.timestamp, "MMM D, YYYY hh:mm:ss A");
+      rainSeries.push({ x: ts, y: val.value });
     });
 
     chartData.datasets[1].data = rainSeries;
